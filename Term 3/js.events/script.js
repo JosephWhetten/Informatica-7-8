04d21aefb357b1b2cc584bbe248b9e92a07a3da3
JosephWhetten <joewhetten21408@gmail.com>
2026-04-22 20:19:21 +0000

    finished articles of faith

---
+++ Term 3/js.events/script.js
@@ -0,0 +1,11 @@
+function articleprint4() {
+    alert("We believe that the first principles and ordinances of the Gospel are: first, Faith in the Lord Jesus Christ; second, Repentance; third, baptism by immersion for the remission of sins; fourth, Laying on of hands for the gift of the Holy Ghost.");
+}
+
+function articleprint7() {
+    alert("We believe in the gift of tongues, prophecy, revelation, visions, healing, interpretation of tongues, and so forth.")
+}
+
+function articleprint11() {
+    alert("We claim the privilege of worshiping Almighty God according to the dictates of our own conscience, and allow all men the same privilege, let them worship how, where, or what they may.")
+}
