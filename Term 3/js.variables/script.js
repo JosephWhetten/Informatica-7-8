2314ede3bbc60e61f01fee04b0675c8398ea92ed
JosephWhetten <joewhetten21408@gmail.com>
2026-04-23 17:15:43 +0000

    workig

---
+++ Term 3/js.variables/script.js
@@ -0,0 +1,32 @@
+function function1() {
+    const name = "Joseph Whetten";
+    const color = "blue";
+    let location = "Colonia Juarez";
+    let mood = "bored";
+
+    console.log("Name: "+name);
+    console.log("Favorite color: "+color);
+    console.log("Location: "+location);
+    console.log("Mood: "+mood);
+
+    mood = "happy";
+    console.log("Mood is now: "+mood);
+}
+
+function function2() {
+    let companyName = "Froyo";
+    const foundingYear = 1970;
+    let isActive = true;
+    let fundingAmount;
+
+    console.log("Company Name: "+companyName);
+    console.log("Founding Year: "+foundingYear);
+    console.log("Is Active: "+isActive);
+    console.log("Funding Amount: "+fundingAmount);
+}
+
+function function3() {
+    i = 103;
+    i = (((i*2)+10)/2)-i
+    alert("103 times 2 plus 10 divided by 2 minus 103 is: "+i);
+}
