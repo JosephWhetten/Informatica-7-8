bc5a656faf934950af7e62e9121d384c9192ec58
JosephWhetten <joewhetten21408@gmail.com>
2026-04-28 16:39:29 +0000

    worked more on coin flip

--- Term 3/js.coin/script.js
+++ Term 3/js.coin/script.js
@@ -1,10 +1,24 @@
 function coinflip() {
     num = Math.floor(Math.random() * 2);
     console.log(num);
+
+
+
     if (num == 0) {
         document.querySelector("h2").innerHTML = "Heads";
+        const para = document.createElement("p");
+        const node = document.createTextNode("Heads");
+
+        para.appendChild(node);
+        document.querySelector("body").appendChild(para);
     }
     else {
         document.querySelector("h2").innerHTML = "Tails";
+        const para = document.createElement("p");
+        const node = document.createTextNode("Tails");
+
+        para.appendChild(node);
+        document.querySelector("body").appendChild(para);
+
     }
 }
