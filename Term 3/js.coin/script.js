933058ab864f764e676b2fc873b025e2d9ef9013
JosephWhetten <joewhetten21408@gmail.com>
2026-04-30 19:55:54 +0000

    finished js.coin

--- Term 3/js.coin/script.js
+++ Term 3/js.coin/script.js
@@ -1,27 +1,44 @@
 function coinflip(choice) {
         num = Math.floor(Math.random() * 2);
-            console.log(num);
+        console.log(num);
 
 
 
-                if (num == 0) {
-                        document.querySelector("h2").innerHTML = "Heads";
-                                const para = document.createElement("p");
-                                        const node = document.createTextNode("Heads");
+        if (num == 0) {
+                document.querySelector("h2").innerHTML = "Heads";
+                if (choice == 'heads') {
+                        const para = document.createElement("p");
+                        const node = document.createTextNode("You got it right!");
 
-                                                para.appendChild(node);
-                                                        document.querySelector("body").appendChild(para);
+                        para.appendChild(node);
+                        document.querySelector("body").appendChild(para);
+                }
+                else {
+                        const para = document.createElement("p");
+                        const node = document.createTextNode("You got it wrong...");
 
-                                                            }
-                                                                else {
-                                                                        document.querySelector("h2").innerHTML = "Tails";
-                                                                                const para = document.createElement("p");
-                                                                                        const node = document.createTextNode("Tails");
+                        para.appendChild(node);
+                        document.querySelector("body").appendChild(para);
+                }
 
-                                                                                                para.appendChild(node);
-                                                                                                        document.querySelector("body").appendChild(para);
 
-                                                                                                            }
-                                                                                                            }
-                                                                                                            
+        }
+        else {
+                document.querySelector("h2").innerHTML = "Tails";
+                if (choice == 'tails') {
+                        const para = document.createElement("p");
+                        const node = document.createTextNode("You got it right!");
+
+                        para.appendChild(node);
+                        document.querySelector("body").appendChild(para);
+                }
+                else {
+                        const para = document.createElement("p");
+                        const node = document.createTextNode("You got it wrong...");
+
+                        para.appendChild(node);
+                        document.querySelector("body").appendChild(para);
+                }
+
+        }
 }
