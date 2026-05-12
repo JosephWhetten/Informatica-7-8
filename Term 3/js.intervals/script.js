59d20ca892f45a402cdec07921ac5beadf67095e
JosephWhetten <joewhetten21408@gmail.com>
2026-05-12 16:39:02 +0000

    working more on interval.js

--- Term 3/js.intervals/script.js
+++ Term 3/js.intervals/script.js
@@ -1,36 +1,49 @@
-// let counter = 0;
-
-// function count() {
-//     counter++;
-//     document.querySelector('h1').innerHTML = counter;
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.querySelector('button').onclick = function(){
-//         setInterval(count, 1000);   // Adds another number to the counter every 1000 milliseconds
-//         document.querySelector('button').disabled = true;
-//     };
-// });
-
-let timer = 60;
-const alarm = new Audio("alarm.mp3");
+let timer;
+const alarm = new Audio("bruh.mp3");
 
 function time() {
-    if (timer != 58) {
+    if (timer > 0) {
         timer--;
-        document.querySelector("h1").innerHTML = timer;
-    }
-    if (timer == 58) {
-        alarm.play();
-    }
+        if (timer > 9) {
+            document.querySelector("h1").innerHTML = "00:" + timer;
+        }
+        else {
+            document.querySelector("h1").innerHTML = "00:0" + timer;
+        }
 
+    }
+    if (timer == 0) {
+        alarm.play();
+        timer--;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("button").onclick = function(){
-        setInterval(time, 1000);
-        document.querySelector("button").disabled = true;
+    document.querySelector("#start").disabled = true;
+
+    document.querySelector("#thirty").onclick = function(){
+        timer=30;
+        document.querySelector("h1").innerHTML = "00:30";
+        document.querySelector("#start").disabled = false;
+    }
+
+    document.querySelector("#sixty").onclick = function(){
+        timer=60;
+        document.querySelector("h1").innerHTML = "00:60";
+        document.querySelector("#start").disabled = false;
+    }
+
+    document.querySelector("#fivem").onclick = function(){
+        timer=300;
+        document.querySelector("h1").innerHTML = "05:00";
+        document.querySelector("#start").disabled = false;
+    }
+
+    document.querySelector("#start").onclick = function(){
+        setInterval(time, 1000);    // Executes the function "time()" every 1000 milliseconds
+        document.querySelector("#start").disabled = true;
+        document.querySelector("#thirty").disabled = true;
+        document.querySelector("#sixty").disabled = true;
+        document.querySelector("#fivem").disabled = true;
     };
 })
-
-
